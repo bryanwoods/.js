da8fe18ede4c77452eef4cc381dd7eda0247e28f
Bryan Woods <pair+bryan@howaboutwe.com>
2012-10-26 16:08:44 -0400

    Adds some howaboutwe.com.js improvmements
    * Better handling of CI and non-Rails environments

--- howaboutwe.com.js
+++ howaboutwe.com.js
@@ -1,7 +1,26 @@
+var environment = function() {
+  var result,
+      subdomain = window.location.host.split(".")[0];
+
+  var subdomainMap = {
+    "www": "PRODUCTION",
+    "local": "DEVELOPMENT"
+  };
+
+  if (Object.keys(subdomainMap).indexOf(subdomain) > -1) {
+    result = subdomainMap[subdomain];
+  } else {
+    result = subdomain;
+  }
+
+  return result.toUpperCase();
+};
+
 $(function() {
   $('body').prepend(
     "<center>" +
-    "<h1 style='color:red;'>YOU ARE CURRENTLY ON PRODUCTION</h1>" +
+    "<h1 style='color:red;'>YOU ARE CURRENTLY ON " + environment() + "</h1>" +
     "</center>"
   );
 });
+
