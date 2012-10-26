75b1c0e7c7489108c7eaed415ddacdbd5e87bda2
Bryan Woods <pair+bryan@howaboutwe.com>
2012-10-26 16:09:10 -0400

    Improves facebook.com.js
    * Fixes for various infinite loop and share button headaches

--- facebook.com.js
+++ facebook.com.js
@@ -1,7 +1,13 @@
-(function() {
-  $(function() {
-    if(confirm("Do you really want to be here?") === false) {
-      window.location = "http://lambda-the-ultimate.org/";
-    }
-  });
-})();
+$(function() {
+  if (window.location.href !== "http://www.facebook.com/home.php") {
+    return;
+  }
+
+  var wantsToBeHere = function() {
+    return confirm("Do you really want to be here?");
+  };
+
+  if(!wantsToBeHere()) {
+    window.location = "http://lambda-the-ultimate.org/";
+  }
+});
