57413ab9ff45157af2957b6645c1adc148a0e181
Bryan Woods <bryanwoods4e@gmail.com>
2013-03-17 17:59:15 -0300

    Periodically Checks TheStrangeLoop.com for a video

---
+++ thestrangeloop.com.js
@@ -0,0 +1,26 @@
+(function() {
+  var title = "The Higher Order Rubyist";
+
+  videoAvailable = function(title) {
+    return $('td a').filter(function(i, el) {
+      return $(el).text() === title;
+    }).length > 0;
+  };
+
+  var checkAvailability = function(title, callback) {
+    if (videoAvailable(title)) {
+      alert("ZOMG IT'S FINALLY HERE!!!");
+      return false;
+    } else {
+      console.log("Bummer, '" + title + "' still hasn't been uploaded.");
+
+      setTimeout(function() {
+        document.location.reload();
+      }, 60000);
+    }
+  };
+
+  $(function() {
+    checkAvailability(title);
+  });
+})();
